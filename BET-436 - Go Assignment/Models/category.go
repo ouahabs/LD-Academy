9557d44d229f25ad6428e9d5f8bd72191bc6d89e
habs <abenchikh6@gmail.com>
2023-03-26 14:44:31 +0100

    fix: added helper data-access find functions

--- BET-436 - Go Assignment/Models/category.go
+++ BET-436 - Go Assignment/Models/category.go
@@ -1,6 +1,30 @@
 package Models
 
+import db "Academy/Database"
+
 type Category struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
+}
+
+func (category Category) FindAll() ([]Category, error) {
+	var categories []Category
+
+	rows, err := db.Connection.Query("SELECT * FROM Categories")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var category Category
+		if err := rows.Scan(&category.ID, &category.Name); err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+
+	return categories, nil
 }
--- BET-436 - Go Assignment/Models/shop.go
+++ BET-436 - Go Assignment/Models/shop.go
@@ -1,9 +1,28 @@
 package Models
 
+import (
+	db "Academy/Database"
+	"database/sql"
+)
+
 type Shop struct {
 	ID           string `json:"id"`
 	Name         string `json:"name"`
 	Address      string `json:address`
 	Password     string `json:password`
 	PasswordHash string `json:hash`
+}
+
+func (shop *Shop) Find(name string) (bool, error) {
+
+	row := db.Connection.QueryRow("SELECT * FROM Shops WHERE name = ?", name)
+
+	if err := row.Scan(&shop.ID, &shop.Name, &shop.Address, &shop.Password, &shop.PasswordHash); err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return true, nil
 }
