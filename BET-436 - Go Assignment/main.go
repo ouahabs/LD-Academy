b74014758dac6073c30bc25d7bbd1b6df5018f63
habs <abenchikh6@gmail.com>
2023-03-26 14:46:20 +0100

    fix: fixed routing and included middleware

--- BET-436 - Go Assignment/main.go
+++ BET-436 - Go Assignment/main.go
@@ -5,28 +5,29 @@ import (
 
 	"Academy/Controllers"
 	db "Academy/Database"
+	"Academy/Middleware"
 )
 
 func main() {
 	router := gin.Default()
 	db.ConnectToDB()
 
-	//router.GET("/signup", Controllers.SignUp)
-	// router.GET("/login", Controllers.LogIn)
+	router.POST("/signup", Controllers.SignUp)
+	router.POST("/login", Controllers.LogIn)
 
-	router.GET("/shops", Controllers.GetShops)
+	router.GET("/shops", Middleware.VerifyAuth(), Controllers.GetShops)
 	router.GET("/shops/:id", Controllers.GetShopById)
-	router.PUT("/shops/:id", Controllers.UpdateShop)
-	router.DELETE("/shops/:id", Controllers.DeleteShop)
+	router.PUT("/shops/:id", Middleware.VerifyAuth(), Controllers.UpdateShop)
+	router.DELETE("/shops/:id", Middleware.VerifyAuth(), Controllers.DeleteShop)
 
-	// router.GET("/products", Controllers.GetProducts)
-	// router.POST("/products", Controllers.AddProduct)
+	router.GET("/products", Controllers.GetProducts)
+	router.POST("/products", Middleware.VerifyAuth(), Controllers.AddProduct)
 
-	// router.GET("/products/:id", Controllers.GetProductById)
-	// router.PUT("/products/:id", Controllers.UpdateProduct)
-	// router.DELETE("/products/:id", Controllers.DeleteProduct)
+	router.GET("/products/:id", Controllers.GetProductById)
+	router.PUT("/products/:id", Middleware.VerifyAuth(), Controllers.UpdateProduct)
+	router.DELETE("/products/:id", Middleware.VerifyAuth(), Controllers.DeleteProduct)
 
-	// router.GET("/categories", Controllers.GetCategories)
+	router.GET("/categories", Controllers.GetCategories)
 
 	router.Run("localhost:8080")
 }
